cbeb90a05b1bd03bcc111aaf4bf5657b8e5a7e14
Alexey Kasyanchuk <degitx@gmail.com>
2020-09-15 17:14:30 +0300

    liberty cross compilation

---
+++ toolchain.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_SYSROOT /home/deg/Projects/zodiac/onemw/build-brcm97449svms-refboard/tmp/sysroots/dcx960-debug)
+set(CMAKE_C_COMPILER /home/deg/Projects/zodiac/onemw/build-brcm97449svms-refboard/tmp/sysroots/x86_64-linux/usr/bin/arm-rdk-linux-gnueabi/arm-rdk-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER /home/deg/Projects/zodiac/onemw/build-brcm97449svms-refboard/tmp/sysroots/x86_64-linux/usr/bin/arm-rdk-linux-gnueabi/arm-rdk-linux-gnueabi-g++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
