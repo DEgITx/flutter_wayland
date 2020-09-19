38ed37f5c95fa744e69560c958a851c4893399da
Alexey Kasyanchuk <degitx@gmail.com>
2020-09-19 23:11:48 +0300

    setup cross-tools

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,9 +1,12 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_SYSROOT /home/deg/Projects/zodiac/onemw/build-brcm97449svms-refboard/tmp/sysroots/dcx960-debug)
-set(CMAKE_C_COMPILER /home/deg/Projects/zodiac/onemw/build-brcm97449svms-refboard/tmp/sysroots/x86_64-linux/usr/bin/arm-rdk-linux-gnueabi/arm-rdk-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER /home/deg/Projects/zodiac/onemw/build-brcm97449svms-refboard/tmp/sysroots/x86_64-linux/usr/bin/arm-rdk-linux-gnueabi/arm-rdk-linux-gnueabi-g++)
+set(LIBERTY_BUILD_DIR /home/deg/Projects/zodiac_apollo/onemw/build-brcm972554pck-refboard)
+set(LIBERTY_TARGET_PLATFORM apollo)
+
+set(CMAKE_SYSROOT ${LIBERTY_BUILD_DIR}/tmp/sysroots/${LIBERTY_TARGET_PLATFORM}-debug)
+set(CMAKE_C_COMPILER ${LIBERTY_BUILD_DIR}/tmp/sysroots/x86_64-linux/usr/bin/arm-rdk-linux-gnueabi/arm-rdk-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER ${LIBERTY_BUILD_DIR}/tmp/sysroots/x86_64-linux/usr/bin/arm-rdk-linux-gnueabi/arm-rdk-linux-gnueabi-g++)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
