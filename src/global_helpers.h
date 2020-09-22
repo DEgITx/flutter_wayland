31885f0ce2ea6a64b02777a6af4c7ab6a4c14c0e
Alexey Kasyanchuk <degitx@gmail.com>
2020-09-22 14:59:57 +0300

    display size get from weston on RDK

---
+++ src/global_helpers.h
@@ -0,0 +1,24 @@
+#ifndef __GLOBALHELPER_H__
+#define __GLOBALHELPER_H__
+
+#include <string>
+#include <vector>
+
+namespace flutter {
+    static std::vector<std::string> splitStr (std::string s, std::string delimiter) {
+        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+        std::string token;
+        std::vector<std::string> res;
+
+        while ((pos_end = s.find (delimiter, pos_start)) != std::string::npos) {
+            token = s.substr (pos_start, pos_end - pos_start);
+            pos_start = pos_end + delim_len;
+            res.push_back (token);
+        }
+
+        res.push_back (s.substr (pos_start));
+        return res;
+    }
+}
+
+#endif
